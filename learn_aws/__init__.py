af744330b2f01845c6b0b4228668b61ee5f5c3ef
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-08-11 15:41:53 -0400

    c

--- learn_aws/__init__.py
+++ learn_aws/__init__.py
@@ -2,6 +2,11 @@
 
 from ._version import __version__
 
+__short_description__ = "AWS Knowledge from My AWS Career since 2011."
+__license__ = "MIT"
 __author__ = "Sanhe Hu"
 __author_email__ = "husanhe@gmail.com"
+__maintainer__ = "Sanhe Hu"
+__maintainer_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
 __chore__ = "6c65bebb1c8a1138f2bc40cabc73a245"
